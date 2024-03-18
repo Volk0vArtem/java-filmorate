05657981deda355986aed3c25df892efd43d18b0
Artem <artem33volkov@gmail.com>
2024-03-18 22:24:05 +0300

    style

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class ValidationException extends Exception{
-    public ValidationException(String s){
+public class ValidationException extends Exception {
+    public ValidationException(String s) {
         super(s);
     }
 }
